b8c136db04d14eb879f80a0dbb8be2fdf439c6f2
netoxdw <neto.guevara.tapia@gmail.com>
2023-12-26 21:40:54 -0800

    instalando import-export al proyecto

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,32 @@
 from django.contrib import admin
 from .models import Categoria, Autor
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 # user: userblog
 # password: nosewe93
 
 # Register your models here.
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+
+class CategoriaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields = ['nombre']
     list_display = ('nombre', 'estado', 'fecha_creacion')
-admin.site.register(Categoria, CategoriaAdmin)
+    resource_class = CategoriaResource
 
-class AutorAdmin(admin.ModelAdmin):
+
+class AutorResource(resources.ModelResource):
+    class Meta: 
+        model = Autor
+
+
+class AutorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields = ['nombres', 'apellidos', 'email']
     list_display = ('nombres', 'apellidos', 'email')
+    resource_class = AutorResource
 
 admin.site.register(Autor, AutorAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
