eeec2373ae097178c897c901dda59eda8e12361b
netoxdw <neto.guevara.tapia@gmail.com>
2023-12-26 17:06:37 -0800

    Creando modelos Categoria, Autor y superusuer

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Autor
+# user: userblog
+# password: nosewe93
 
 # Register your models here.
+# class CategoriaAdmin(admin.AdminSite):
+admin.site.register(Categoria)
+admin.site.register(Autor)
